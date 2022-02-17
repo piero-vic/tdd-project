6813f3d5e019057def9dd240a1cc613ee04ec62b
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-02-17 11:22:45 -0500

    Add test for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,4 +1,10 @@
 require_relative '../solver'
 
 describe Solver do
+  solver = Solver.new
+  describe "#factorial" do
+    it 'should return the factorial of N' do
+      expect(solver.factorial(5)).to eql 120
+    end
+  end
 end
