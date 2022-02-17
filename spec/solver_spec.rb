8ff7d79784e6e830ea733addea6dc3b828d39b9d
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-02-17 11:31:55 -0500

    Write test for all cases of factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,9 +2,23 @@ require_relative '../solver'
 
 describe Solver do
   solver = Solver.new
-  describe "#factorial" do
-    it 'should return the factorial of N' do
-      expect(solver.factorial(5)).to eql 120
+  describe '#factorial' do
+    context '0 or 1 as argument' do
+      it 'should return 1' do
+        expect(solver.factorial(0)).to eql 1
+        expect(solver.factorial(1)).to eql 1
+      end
+    end
+    context 'negative number as argument' do
+      it 'should raise and error' do
+        expect { solver.factorial(-1) }.to raise_error('Negative number not accepted')
+      end
+    end
+    context 'any other integer as argument' do
+      it 'should return the factorial of the integer' do
+        expect(solver.factorial(5)).to eql 120
+        expect(solver.factorial(10)).to eql 3628800
+      end
     end
   end
 end
