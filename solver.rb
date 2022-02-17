ee0d21f34ea7ae7c4e5d3eb1b49bb905d85d89cd
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2022-02-17 11:52:22 -0500

    Implement factorial method

--- solver.rb
+++ solver.rb
@@ -1,3 +1,8 @@
 class Solver
-  
+  def factorial(number)
+    raise 'Negative number not accepted' if number.negative?
+    return 1 if [0, 1].include?(number)
+
+    number * factorial(number - 1)
+  end
 end
